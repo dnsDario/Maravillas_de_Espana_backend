24380a77c7905ab51b711ba79f99a572fdd46049
Darioooo <darionietosastre@gmail.com>
2024-05-14 17:14:24 +0200

    se crea variable de entorno con la URL del front de Vercel

--- index.js
+++ index.js
@@ -9,10 +9,11 @@ require("dotenv").config();
 const app = express()
 
 const port = process.env.PORT || 3000;
+const host = process.env.HOST || process.env.FRONT_URL_VERCEL;
 
 var corsOptions = {
   //config. para cors abra conexion a la ruta elegida
-  origin: 'https://marvillas-de-espana-frontend.vercel.app' || process.env.HOST,
+  origin: host,
   optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
